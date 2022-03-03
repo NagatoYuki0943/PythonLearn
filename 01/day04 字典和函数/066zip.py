9db4abdbe2811b5daab6fe42351cab3e093fda00
P775TM1 <lihongtu12138@outlook.com>
2022-03-03 21:34:56 +0800

    zip更新用法

--- 01/day04 字典和函数/066zip.py
+++ 01/day04 字典和函数/066zip.py
@@ -11,18 +11,28 @@ l1 = [1, 2, 'a', True]
 
 z1 = zip(l1)
 print(z1)  # <zip object at 0x0000013A7868FE88>
-print(list(z1))  # [(1,), (2,), ('a',), (True,)]
+print(list(z1))     # [(1,), (2,), ('a',), (True,)]
 
 
 d1 = {'a': 1, 'b': 2, 'c': 3}
 z2 = zip(d1.items())
-print(list(z2))  # [(('a', 1),), (('b', 2),), (('c', 3),)]
+print(list(z2))     # [(('a', 1),), (('b', 2),), (('c', 3),)]
 
 
 z3 = zip(d1.keys(), d1.values())
-print(dict(z3))  # {'a': 1, 'b': 2, 'c': 3}
+print(dict(z3))     # {'a': 1, 'b': 2, 'c': 3}
 
 
 # 快速交换key和value
 d4 = zip(d1.values(), d1.keys())
-print(dict(d4))  # {1: 'a', 2: 'b', 3: 'c'}
+print(dict(d4))     # {1: 'a', 2: 'b', 3: 'c'}
+
+
+z4 = zip([[1, "a"], [2, "b"], [3, "c"]])
+print(tuple(z4))    # (([1, 'a'],), ([2, 'b'],), ([3, 'c'],))
+
+
+# zip * 将数据分为两组
+z5 = zip(*[[1, "a"], [2, "b"], [3, "c"]])
+print(*[[1, "a"], [2, "b"], [3, "c"]])  # [1, 'a'] [2, 'b'] [3, 'c']
+print(tuple(z5))    # ((1, 2, 3), ('a', 'b', 'c'))
