7aa76ba369da47e2e5893b53726f576ce4bc292f
P775TM1 <lihongtu12138@outlook.com>
2022-05-16 12:15:46 +0800

    tqdm更新

--- 01/day24 tqdm/tqdm_.py
+++ 01/day24 tqdm/tqdm_.py
@@ -1,15 +1,16 @@
 from tqdm import tqdm
 import time
+import random
 
 for epoch in range(100):
-    pbar = tqdm(total=1000,desc=f'Epoch {epoch + 1}/100', postfix=dict, mininterval=0.1)
+    # 创建进度条 长度      描述                           添加其余数据的方式 显示间隔
+    pbar = tqdm(total=100,desc=f'Epoch {epoch + 1}/100', postfix=dict, mininterval=0.1)
 
-    for i in range(1000):
-        time.sleep(0.001)
-        pbar.update(1)
+    for i in range(100):
+        time.sleep(0.1)
+        pbar.update(1)  # 进度条+1
         # 在每一行后面添加数值
-        pbar.set_postfix(**{'total_loss': 1,
-                            'accuracy'  : 2,
-                            'lr'        : 3})
-        # Epoch 3/100:  26%|███            | 3227/12500 [00:06<00:24, 379.15it/s, accuracy=2, lr=3, total_loss=1]
-
+        pbar.set_postfix(**{'total_loss': random.random(),
+                            'accuracy'  : random.random(),
+                            'lr'        : random.random()})
+        # Epoch 2/100:  71%|█████████████     | 71/100 [00:07<00:03,  9.10it/s, accuracy=0.958, lr=0.191, total_loss=0.893]
