85edabb9e0d6c4ec7b9fdf78722ac4975c9f1122
P775TM1 <lihongtu12138@outlook.com>
2022-06-21 18:15:58 +0800

    getattr使用

---
+++ 01/day34 getattr/getattr.py
@@ -0,0 +1,67 @@
+#------------------------------------------#
+#   getattr() 函数用于返回一个对象属性值。
+#   getattr 语法：
+#       getattr(object, name[, default])
+#   参数
+#       object  -- 对象。
+#       name    -- 字符串，对象属性。
+#       default -- 默认返回值，如果不提供该参数，在没有对应属性时，将触发 AttributeError。
+#------------------------------------------#
+
+class Person(object):
+    inherit = 'DNA'
+    def __init__(self, age) -> None:
+        self.age = age
+
+# 可以获取 类变量 和 实例变量
+person = Person(12)
+print(getattr(person, 'inherit'))       # DNA
+print(getattr(person, 'age'))           # 12
+print(getattr(person, 'name', 'may'))   # may
+print('\n')
+
+
+
+class Calc(object):
+    def add(self, *args):
+        return sum(args)
+
+    def sub(self, *args):
+        res = args[0]
+        for i in args[1:]:
+            res -= i
+        return res
+
+    def mul(self, *args):
+        res = args[0]
+        for i in args[1:]:
+            res *= i
+        return res
+
+    def div(self, *args):
+        res = args[0]
+        for i in args[1:]:
+            assert i != 0, f"second argument {i} can't be zero"
+            res /= i
+        return res
+
+clac = Calc()
+a = [4, 3, 2, 1]
+
+print(clac.add(*a))     # 10
+print(clac.sub(*a))     # -2
+print(clac.mul(*a))     # 0
+print(clac.div(*a))     # 0.6666666666666666
+print('\n')
+
+#------------------------------------------#
+#   getattr获取的方法可以直接使用
+#------------------------------------------#
+add = getattr(clac, 'add')
+sub = getattr(clac, 'sub')
+mul = getattr(clac, 'mul')
+div = getattr(clac, 'div')
+print(add(*a))          # 10
+print(sub(*a))          # -2
+print(mul(*a))          # 0
+print(div(*a))          # 0.6666666666666666
