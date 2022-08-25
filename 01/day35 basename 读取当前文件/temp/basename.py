99242d9d1bb312bc3c5e5dbe3c84c9c91b632cb4
P775TM1 <lihongtu12138@outlook.com>
2022-08-25 21:42:43 +0800

    python读取当前文件

---
+++ 01/day35 basename 读取当前文件/temp/basename.py
@@ -0,0 +1,13 @@
+import os,sys
+os.chdir(sys.path[0])
+
+file_name=os.path.basename(__file__)
+print(file_name)    # basename.py
+
+
+with open(file_name, 'r', encoding='utf-8') as f:
+    txt = f.read()
+
+
+with open('temp/'+file_name, 'w', encoding='utf-8') as f:
+    f.write(txt)
