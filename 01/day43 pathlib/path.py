e8271f829536d24e07b6fe241283db3521eeb63e
NagatoYuki0943 <72508155+NagatoYuki0943@users.noreply.github.com>
2023-06-30 13:19:13 +0800

    Update path.py

--- 01/day43 pathlib/path.py
+++ 01/day43 pathlib/path.py
@@ -1,6 +1,17 @@
 import os
-import shutil
 import pathlib
 
-print(pathlib.Path(__file__))
-print(os.path.basename(__file__))
+
+file = pathlib.Path(__file__)
+print(file)                         # c:\Users\Administrator\Desktop\self\fastapi_learn\path.py
+print(file.name)                    # path.py
+print(file.stem)                    # path
+print(file.suffix)                  # .py
+print(file.parent)                  # c:\Users\Administrator\Desktop\self\fastapi_learn
+print(file.is_absolute())           # True
+print(file.absolute())              # c:\Users\Administrator\Desktop\self\fastapi_learn\path.py
+print(file.resolve())               # C:\Users\Administrator\Desktop\self\fastapi_learn\path.py
+print(file.is_dir())                # False
+print(file.is_file())               # True
+
+print(os.path.basename(__file__))   # path.py
