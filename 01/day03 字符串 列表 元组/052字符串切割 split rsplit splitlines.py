68c50e3adcac98be9ec2497010c0573c679ce8b4
NagatoYuki0943 <72508155+NagatoYuki0943@users.noreply.github.com>
2023-09-21 09:32:14 +0800

    Update 052字符串切割 split rsplit splitlines.py

--- 01/day03 字符串 列表 元组/052字符串切割 split rsplit splitlines.py
+++ 01/day03 字符串 列表 元组/052字符串切割 split rsplit splitlines.py
@@ -7,7 +7,10 @@ substr: 按照什么内容分隔字符串,默认是空白字符: 空格,tab,\n
 count:  切割次数,切割出的数量 = count + 1,默认全部切割
 返回值: 列表[],切割的数据就没有了
 
-有 rsplit 方法,从右到左
+rsplit 方法,从右到左
+
+splitlines 按照行分割
+
 '''
 my_str = 'hello world itcast and itcastcpp'
 
@@ -30,11 +33,16 @@ res = my_str.rsplit('itcast', 1)
 print(res)  # ['hello world itcast and ', 'cpp']
 
 # splitlines 按照行分隔，返回一个包含各行作为元素的列表
-my_str= 'Hello\nWorld\n'            # splitlines可以去除单个空行
-print(my_str.splitlines())          # ['Hello', 'World']
+my_str= 'Hello\nWorld\n'                # splitlines可以去除单个空行
+print(my_str.splitlines())              # ['Hello', 'World']
+print(my_str.splitlines(keepends=True)) # ['Hello\n', 'World\n']    keepends会保留结尾
 
-my_str= 'Hello\nWorld\n\n'          # 多个空行无法去除
-print(my_str.splitlines())          # ['Hello', 'World', '']
+my_str= 'Hello\nWorld\n\n'              # 多个空行无法去除
+print(my_str.splitlines())              # ['Hello', 'World', '']
 
 # split可以除去多个空行
-print(my_str.split())               # ['Hello', 'World']
+print(my_str.split())                   # ['Hello', 'World']
+
+my_str= 'Hello World\nnihao zaima\n'
+# split可以除去多个空行,但是会将一行中按照空格等分开
+print(my_str.split())                   # ['Hello', 'World', 'nihao', 'zaima']
