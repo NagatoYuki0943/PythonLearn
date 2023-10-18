63fa9a816b469e394bc89a96b7e81c2bfe174761
Yuki <workcomputer.com>
2023-10-18 16:42:37 +0800

    加密 -> hashlib

--- 01/hashlib/hash.py
+++ 01/hashlib/hash.py
@@ -25,3 +25,4 @@ print(res)
 res = hashlib.sha512(msg.encode('utf-8')).hexdigest()
 print(res)
 # 309ecc489c12d6eb4cc40f50c902f2b4d0ed77ee511a7c7a9bcd3ca86d4cd86f989dd35bc5ff499670da34255b45b0cfd830e81f605dcf7dc5542e93ae9cd76f  # 128个长度
+
