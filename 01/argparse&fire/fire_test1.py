239cea5a04e74d15c0248559555410e0e0850b86
Yuki <workcomputer.com>
2023-10-18 16:22:17 +0800

    update fire

--- 01/argparse&fire/fire_test1.py
+++ 01/argparse&fire/fire_test1.py
@@ -1,4 +1,15 @@
-# https://zhuanlan.zhihu.com/p/100459723
+"""
+https://zhuanlan.zhihu.com/p/100459723
+
+fire 可以根据任何 Python 对象自动生成命令行接口。它有如下特性：
+
+· 能以简单的方式生成 CLI
+· 是一个开发和调试 Python 代码的实用工具
+· 能将现存代码或别人的代码转换为 CLI
+· 使得在 Bash 和 Python 间的转换变得更容易
+· 通过预先为 REPL 设置所需的模块和变量，使得实用 REPL 更加容易
+"""
+
 import fire
 
 
@@ -20,7 +31,7 @@ def train_from_folder(
 
 if __name__ == "__main__":
     fire.Fire(train_from_folder)
-    # > python fire_test.py --data ./datasets --new True --optimizer lion --generate_types ema --log_dir ./logs
+    # > python fire_test1.py --data ./datasets --new True --optimizer lion --generate_types ema --log_dir ./logs
     # data: ./datasets
     # new: True
     # image_size: 256
