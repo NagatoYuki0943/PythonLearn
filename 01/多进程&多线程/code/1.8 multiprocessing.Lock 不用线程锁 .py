bd00484b0896120a49ecfa47f100272025d60e0e
Yuki <workcomputer.com>
2023-11-09 16:09:20 +0800

    multiprocessing threading lock

---
+++ 01/多进程&多线程/code/1.8 multiprocessing.Lock 不用线程锁 .py
@@ -0,0 +1,49 @@
+from multiprocessing import Process, Lock
+
+
+A = 0
+
+
+def job1():
+    global A
+    for i in range(10):
+        A += 1
+        print('job1',A)
+
+
+def job2():
+    global A
+    for i in range(10):
+        A += 10
+        print('job2',A)
+
+
+if __name__== '__main__':
+    t1 = Process(target=job1)
+    t2 = Process(target=job2)
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+
+    # 结果很清晰,理应很乱,在多线程中是乱的,在进程中就很正常,和使用了lock一样,原因不明
+    # job1 1
+    # job1 2
+    # job1 3
+    # job1 4
+    # job1 5
+    # job1 6
+    # job1 7
+    # job1 8
+    # job1 9
+    # job1 10
+    # job2 10
+    # job2 20
+    # job2 30
+    # job2 40
+    # job2 50
+    # job2 60
+    # job2 70
+    # job2 80
+    # job2 90
+    # job2 100
---
+++ 01/多进程&多线程/code/2.5 threading.Lock 不用线程锁.py
@@ -0,0 +1,46 @@
+from threading import Thread, Lock
+
+def job1():
+    global A
+    for i in range(10):
+        A += 1
+        print('job1',A)
+
+
+def job2():
+    global A
+    for i in range(10):
+        A += 10
+        print('job2',A)
+
+
+if __name__== '__main__':
+    A = 0
+    t1 = Thread(target=job1)
+    t2 = Thread(target=job2)
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+
+    # 结果很乱,2个线程相互争夺资源,没有顺序
+    # job1 1
+    # job1 2
+    # job2 12
+    # job1 13
+    # job2 23
+    # job1 24
+    # job2 34
+    # job1 35
+    # job2 45
+    # job1 46
+    # job2 56
+    # job1 57
+    # job2 67
+    # job1 68
+    # job2 78
+    # job1 79
+    # job2 89
+    # job1 90
+    # job2 100
+    # job2 110
