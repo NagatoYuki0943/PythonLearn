a15ae4e17a0ec7c6af015a0c4cfaaa5c366ef876
Yuki <workcomputer.com>
2023-12-11 10:14:15 +0800

    add_argument choices

--- 01/argparse&click&fire/argparse_test.py
+++ 01/argparse&click&fire/argparse_test.py
@@ -6,7 +6,7 @@ def parse_opt():
     parser.add_argument('--data', type=str, default='data/coco128.yaml', help='dataset.yaml path')
     parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
     parser.add_argument('--batch-size', type=int, default=1, help='batch size')
-    parser.add_argument('--device', default='cpu', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--device', default='cpu', choices=["cpu", "cuda"], help='cuda or cpu')
     parser.add_argument('--half', action='store_true', help='FP16 half-precision export')
     parser.add_argument('--conf-thres', type=float, default=0.25, help='TF.js NMS: confidence threshold')
     parser.add_argument(
