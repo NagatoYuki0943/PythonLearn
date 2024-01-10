6e5c42723c3f63b4f89deb4b1f08f8bcdfc1ff6a
Yuki <workcomputer.com>
2024-01-10 10:14:04 +0800

    理解 argparse nargs,default,const 参数

--- 01/argparse&click&fire/argparse_test.py
+++ 01/argparse&click&fire/argparse_test.py
@@ -3,16 +3,16 @@ import argparse
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument('version', type=str, help='version, 必须在第一位')
-    parser.add_argument('--data', type=str, default='data/coco128.yaml', help='dataset.yaml path')
-    parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
-    parser.add_argument('--batch-size', type=int, default=1, help='batch size')
+    parser.add_argument('version', type=str, help='version, 必须在第一位,且不需要说明version')
+    parser.add_argument('--weights', type=str, default='yolov5s.pt', help='model.pt path(s)')
+    # default 代表不写 --device 参数时的默认值
     parser.add_argument('--device', default='cpu', choices=["cpu", "cuda"], help='cuda or cpu')
+    # const 代表写 --evolve 但不写参数时的默认值
+    parser.add_argument("--evolve", type=int, nargs="?", const=300, help="evolve hyperparameters for x generations")
     parser.add_argument('--half', action='store_true', help='FP16 half-precision export')
-    parser.add_argument('--conf-thres', type=float, default=0.25, help='TF.js NMS: confidence threshold')
     parser.add_argument(
         '--include',
-        nargs='+',
+        nargs='+',  # n : 参数的绝对个数;  ? : 0或1个参数;  * : 0或所有参数;  + : 至少一个参数;
         default=['torchscript'],
         choices=["torchscript", "onnx", "openvino", "engine", "coreml", "saved_model", "pb", "tflite", "edgetpu", "tfjs", "paddle"],
         help='torchscript, onnx, openvino, engine, coreml, saved_model, pb, tflite, edgetpu, tfjs, paddle')
@@ -23,5 +23,7 @@ def parse_opt():
 if __name__ == '__main__':
     opt = parse_opt()
     print(opt)
-    # > python argparse_test.py v8 --data coco.yaml --weights yolov5x.pt --batch-size 16 --device cuda --half --conf-thres 0.5 --include onnx
-    # Namespace(version='v8', data='coco.yaml', weights=['yolov5x.pt'], batch_size=16, device='cuda', half=True, conf_thres=0.5, include=['onnx'])
+    # > python argparse_test.py v8 --weights yolov5x.pt --device cuda --evolve --include onnx   # 不指定--evolve
+    # Namespace(version='v8', weights='yolov5x.pt', device='cuda', evolve=300, half=False, include=['onnx'])
+    # > python argparse_test.py v8 --weights yolov5x.pt --evolve --include onnx                 # 不写--device
+    # Namespace(version='v8', weights='yolov5x.pt', device='cpu', evolve=300, half=False, include=['onnx'])
