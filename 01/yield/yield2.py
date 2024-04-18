08eebf09fe17a79636ecbf47705549e9fc053e31
yuki <lihongtu12138@outlook.com>
2024-04-18 10:00:07 +0800

    使用yield from

---
+++ 01/yield/yield2.py
@@ -0,0 +1,20 @@
+
+# 生成器函数
+def my_generator():
+    for x in range(10):
+        yield x
+
+# 遍历生成器
+for item in my_generator():
+    print(item, end=" ")
+print()
+
+
+# 生成器函数可以被返回
+def my_generator1():
+    return my_generator()
+
+# 遍历生成器
+for item in my_generator1():
+    print(item, end=" ")
+print()
