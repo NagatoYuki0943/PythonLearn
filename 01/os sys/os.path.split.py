0504c652a583fa51da4a8f36ecfe5479e748ac6d
NagatoYuki0943 <lihongtu12138@outlook.com>
2024-04-22 18:39:04 +0800

    add os.path.split

---
+++ 01/os sys/os.path.split.py
@@ -0,0 +1,7 @@
+import os
+
+parent, filename = os.path.split('./data\\FM docs 2024.3\\JOM_1998_13_4_06_The_Application_of_the_Hardin_Jones-Pauling-.pdf')
+print(parent)
+print(filename)
+# ./data\FM docs 2024.3
+# JOM_1998_13_4_06_The_Application_of_the_Hardin_Jones-Pauling-.pdf
