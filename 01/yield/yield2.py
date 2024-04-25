b674b44e11e3b440b12c8269c3fcd75f80019439
NagatoYuki0943 <lihongtu12138@outlook.com>
2024-04-25 20:01:09 +0800

    compare return , yield from

--- 01/yield/yield2.py
+++ 01/yield/yield2.py
@@ -1,22 +1,43 @@
+"""
+yeild 迭代器可以被返回
+yield from后面加上可迭代对象，他可以把可迭代对象里的每个元素一个一个的yield出来，
+对比yield来说代码更加简洁，结构更加清晰。
+"""
 
-# 生成器函数
-def my_generator():
-    for x in range(10):
-        yield x
+def reader():
+    for i in range(4):
+        yield '<< %s' % i
 
-# 遍历生成器
-for item in my_generator():
-    print(item, end=" ")
+
+def reader_wrapper1():
+    # Manually iterate over data produced by reader
+    return reader()
+
+wrap = reader_wrapper1()
+for i in wrap:
+    print(i, end=" ")
 print()
+# << 0 << 1 << 2 << 3
 
 
-# 生成器函数可以被返回
-def my_generator1():
-    gen = my_generator()
-    print(gen)
-    return gen
+def reader_wrapper2():
+    # Manually iterate over data produced by reader
+    for v in reader():
+        yield v
 
-# 遍历生成器
-for item in my_generator1():
-    print(item, end=" ")
+wrap = reader_wrapper2()
+for i in wrap:
+    print(i, end=" ")
 print()
+# << 0 << 1 << 2 << 3
+
+
+def reader_wrapper3():
+    # Instead of manually iterating over reader(), we can just yield from it.
+    yield from reader()
+
+wrap = reader_wrapper3()
+for i in wrap:
+    print(i, end=" ")
+print()
+# << 0 << 1 << 2 << 3
