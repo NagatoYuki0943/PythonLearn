9e7bf66afeaef70e9b9c3b9f852be198939e9341
NagatoYuki0943 <lihongtu12138@outlook.com>
2024-05-08 21:23:58 +0800

    loguru slower than print

---
+++ 01/loguru_test/loguru_speed.py
@@ -0,0 +1,165 @@
+from loguru import logger
+import time
+import numpy as np
+from dataclasses import dataclass
+
+
+str1 = """query: 上下文:
+<context>
+Eye Pressure Lowering Effect of Vitamin C
+Herschell H. Boyd, M.D.1
+Purpose
+To document the pressure before the use
+of vitamin C and after the daily intake of
+maximum amounts of vitamin C, three times
+a day.
+Methods
+Thirty patients (16 men and 14 women)
+were advised to take three divided doses of
+vitamin C in capsule form each day until
+loose stools occured and then back downslightly from this amount (bowel dosage) for
+a daily intake. Average daily intake for all
+patients was 10 grams per day.
+Eye Pressure Lowering Effect of Vitamin C
+167with 12 grams of vitamin C each day. She
+was a smoker in her early fifties, and she hascontinued to smoke with good reason to
+cease.
+Glaucoma accompanies the cataract pa-
+tients so a daily intake of vitamin C could
+save the country literally in the billions of
+dollars. This would admittedly be detrimen-tal to the financial health of ophthalmolo-
+gists, but of course this is not the issue.
+Ascorbic acid has been reported to be of
+paid by research grants from drug compa-nies and from the National Institute of Health
+in Washington, D.C. These two areas don't
+fund vitamin C. If a source of private moneycould be found, the medical school would be
+pleased to do studies as desired. A private
+foundation may be the salvation of the glau-coma patients, and, of course, the many
+other areas we need to treat.
+Conclusion
+1. Vitamin C does lower the intraocular
+pressure in all patients with elevatedpressure.
+use eye drops to lower the pressure below 20mm of mercury as they refused to take vita-
+min C.
+Conclusion
+In this series of 30 patients there was no
+occasion in which the pressure was not low-ered with vitamin C. All drugs for glaucoma
+are seriously toxic, and vitamin C has no
+toxicity yet known. The patients experiencedmany other good side effects from vitamin C
+such as clearing of sinusitis, allergy symp-
+Journal of Orthomolecular Medicine     Vol. 10, No. 3 & 4, 1995
+166Average Drop in Eye Pressure
+Right Eye: 4.8 mmLeft Eye: 6.3 mm
+Both Eyes: 5.6 mm
+It is an interesting observation that indi-
+viduals who take vitamin C regularly and
+have their intraocular pressure taken on a
+routine basis for their eye exam have pres-sures in the range of 10-13 mm of mercury
+commonly. It has been my experience that I
+have yet to find the first patient with apressure over 20 mm who comes into the
+EARLY EVIDENCE ABOUT VITAMIN C AND THE COMMON COLD
+present paper is that the evidence for this
+protective effect was already moderately strong
+by 1942 (Glazebrook and Thomson; Cowan,
+Diehl, and Baker), and was very strong by 1961.
+Despite the strength of the evidence, which has
+been systematically misrepresented by the
+medical and nutritional authorities, the possible
+value of an increased intake of vitamin C in
+decreasing the amount of suffering and loss of
+awaken to the marvels of vitamin C in treat-
+ing glaucoma!
+Introduction
+Vitamin C has been used since the trans-
+formation of glucose, C
+6H12O6, into vitamin
+1. Eastview Professional Building, 1370 - 116th Avenue
+N.E., Suite 212, Bellevue WA 98004-4679.
+165C, C6H8O6, in the early 1930s. Its use in
+lowering the pressure in glaucoma datesback to 1962 as reported in Experimental
+Eye Research.
+1 Since then there have been
+</context>
+问题:
+<question>Eye Pressure Lowering Effect of Vitamin C</question>
+请使用提供的上下文来回答问题，如果上下文不足请根据自己的知识给出合适的建议(除非用户指定了回答的语言，否则用户使用什么语言就什么语言回答):
+"""
+
+start = time.time()
+for i in range(100):
+    print(str1)
+end = time.time()
+print_time1 = end - start
+
+
+start = time.time()
+for i in range(100):
+    logger.info(str1)
+end = time.time()
+logger_time1 = end - start
+
+
+str2 = ("根据上下文，作者提出了一种通过每日摄入最大量的维生素C来降低眼压的方法。"
+        "作者认为，通过减少每天10克维生素C的摄入量，可以降低眼压，这将对治疗"
+        "和预防眼压性疾病的患者产生巨大的经济效益。作者指出，虽然这些药物可能"
+        "对医生和研究机构构成威胁，但是通过私人资金和基金的资助，可以开展更多"
+        "的研究，以进一步了解维生素C的作用。作者还指出，对于那些患有眼压性疾"
+        "病的患者，每日摄入维生素C可以成为一种有效的治疗手段，并且可能减轻他们的痛苦和痛苦。"
+        "根据这些信息，作者建议，通过每日摄入最大量的维生素C来降低眼压，可以"
+        "减少眼压性疾病的患者的痛苦，同时也可以为医生和研究机构提供更多的研究"
+        "机会。作者强调，虽然这些药物可能对医生和研究机构构成威胁，但是通过"
+        "私人资金和基金的资助，可以开展更多的研究，以进一步了解维生素C的作用。"
+        "因此，作者建议，每日摄入最大量的维生素C来降低眼压，可以减少眼压性"
+        "疾病的患者的痛苦，同时也可以为医生和研究机构提供更多的研究机会。"
+        "作者强调，虽然这些药物可能对医生和研究机构构成威胁，但是通过私人"
+        "资金和基金的资助，可以开展更多的研究，以进一步了解维生素C的作用。")
+
+length = len(str2)
+
+start = time.time()
+for i in range(1, length + 1):
+    print(str2[0:i])
+end = time.time()
+print_time2 = end - start
+
+start = time.time()
+for i in range(1, length + 1):
+    logger.info(str2[0:i])
+end = time.time()
+logger_time2 = end - start
+
+
+# Response(text='很高兴', generate_token_len=10, input_token_len=111, session_id=0, finish_reason=None)
+@dataclass
+class Response:
+    text: str
+    generate_token_len: int
+    input_token_len: int
+    session_id: int
+    finish_reason: str
+
+responses = []
+for s in str2:
+    responses.append(Response(text=s, generate_token_len=10, input_token_len=111, session_id=0, finish_reason=None))
+
+
+start = time.time()
+for response in responses:
+    print(response)
+end = time.time()
+print_time3 = end - start
+
+
+start = time.time()
+for response in responses:
+    logger.info(response)
+end = time.time()
+logger_time3 = end - start
+
+
+print(f"{print_time1 = }")  # 0.4566938877105713
+print(f"{logger_time1 = }") # 0.4743990898132324
+print(f"{print_time2 = }")  # 0.11689352989196777
+print(f"{logger_time2 = }") # 0.23590612411499023
+print(f"{print_time3 = }")  # 0.04466414451599121
+print(f"{logger_time3 = }") # 0.1304488182067871
