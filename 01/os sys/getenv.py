4ad2a994503d81fc7100b3179e18c72ef35364aa
NagatoYuki0943 <lihongtu12138@outlook.com>
2024-05-23 21:11:52 +0800

    update getenv

---
+++ 01/os sys/getenv.py
@@ -0,0 +1,25 @@
+"""https://kimi.moonshot.cn/chat/cp7j4i6bi7sbkj5c306g
+"""
+
+import os
+
+
+"""
+设置临时变量
+
+linux:
+    # 不允许使用空格
+    export HF_TOKEN="abcdefghijklmnopqrstuvwxyz"
+
+powershell:
+    # 设置一个简单的变量
+    $HF_TOKEN = "abcdefghijklmnopqrstuvwxyz"
+
+    # 设置环境变量
+    $env:HF_TOKEN = "abcdefghijklmnopqrstuvwxyz"
+"""
+
+
+hf_token = os.getenv("HF_TOKEN", "")
+print(hf_token)
+# abcdefghijklmnopqrstuvwxyz
