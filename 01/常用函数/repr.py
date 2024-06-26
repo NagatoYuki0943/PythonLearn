e1d1b6382e1d345b6229a7f3c70fa2a41d6da2e4
NagatoYuki0943 <72508155+NagatoYuki0943@users.noreply.github.com>
2024-06-26 09:16:04 +0800

    Update repr.py

--- 01/常用函数/repr.py
+++ 01/常用函数/repr.py
@@ -1,6 +1,7 @@
 """
-repr() 函数将对象转化为供解释器读取的形式。
+repr() 函数将对象转化为供解释器读取的形式，不转义字符。
 """
+import re
 
 
 s = '\nRUNOOB\n'
@@ -13,6 +14,9 @@ print(s)
 print(repr(s))  # '\nRUNOOB\n'
 
 
-dict = {'runoob': 'runoob.com', 'google': 'google.com'};
-print(dict)         # {'runoob': 'runoob.com', 'google': 'google.com'}
-print(repr(dict))   # {'runoob': 'runoob.com', 'google': 'google.com'}
+split_words = ["\n*no reference.*", "\nreferences:"]
+print(split_words)                  # ['\n*no reference.*', '\nreferences:']
+
+split_words = [re.escape(word) for word in split_words]
+print(split_words)                  # ['\\\n\\*no\\ reference\\.\\*', '\\\nreferences:']
+print(repr("|".join(split_words)))  # '\\\n\\*no\\ reference\\.\\*|\\\nreferences:'  # 忽略转义字符
