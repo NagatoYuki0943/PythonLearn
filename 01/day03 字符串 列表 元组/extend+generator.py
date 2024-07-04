eb5aa918a6e35f9bb479cfc34c4d7e82b621595f
NagatoYuki0943 <72508155+NagatoYuki0943@users.noreply.github.com>
2024-07-04 10:55:07 +0800

    Update extend+generator.py

--- 01/day03 字符串 列表 元组/extend+generator.py
+++ 01/day03 字符串 列表 元组/extend+generator.py
@@ -4,9 +4,25 @@ x.extend([x[-1] * 2 for _ in range(3)])
 print(x)
 # [1, 2, 4, 8, 8, 8]
 
+x = [1, 2, 4]
+y = [x[-1] * 2 for _ in range(3)]
+x.extend(y)
+print(type(y))
+# <class 'list'>
+print(x)
+# [1, 2, 4, 8, 8, 8]
+
 
 # extend(generator)
 x = [1, 2, 4]
 x.extend(x[-1] * 2 for _ in range(3))
+print(x)
+# [1, 2, 4, 8, 16, 32]
+
+x = [1, 2, 4]
+y = (x[-1] * 2 for _ in range(3))
+x.extend(y)
+print(type(y))
+# <class 'generator'>
 print(x)
 # [1, 2, 4, 8, 16, 32]
