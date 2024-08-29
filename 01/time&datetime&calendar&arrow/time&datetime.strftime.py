ae3db27d5ef892d818e933b1dc04c50c6874cb09
NagatoYuki0943 <72508155+NagatoYuki0943@users.noreply.github.com>
2024-08-29 14:14:12 +0800

    Update time&datetime.strftime.py

--- 01/time&datetime&calendar&arrow/time&datetime.strftime.py
+++ 01/time&datetime&calendar&arrow/time&datetime.strftime.py
@@ -11,7 +11,7 @@ print(formatted_time)
 
 # 获取当前时间
 now = datetime.now()
-# 格式化时间戳，包含毫秒
+# 格式化时间戳，%f 表示微秒
 formatted_time_with_milliseconds = now.strftime("%Y%m%d-%H%M%S.%f")
 print(formatted_time_with_milliseconds)
 # 20240829-112616.982207
