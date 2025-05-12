6dc9843805978d926f1035ba001ef9d4f82638a2
NagatoYuki0943 <72508155+NagatoYuki0943@users.noreply.github.com>
2025-05-12 10:56:52 +0800

    Update _.py

--- 01/_.py
+++ 01/_.py
@@ -3,8 +3,22 @@ shape = [1, 3, 224, 224]
 
 b, c, h, w = shape
 print(b, c, h, w)  # 1 3 224 224
+print()
 
 
 b, *_ = shape
 print(b)  # 1
 print(_)  # [3, 224, 224]
+print()
+
+
+b, *_, h = shape
+print(b, h)  # 1 224
+print(_)  # [3, 224, 224]
+print()
+
+
+*_, h, w = shape
+print(_)  # [1, 3]
+print(h, w)  # 224 224
+print()
