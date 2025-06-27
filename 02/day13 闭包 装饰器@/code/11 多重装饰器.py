6f187d2c28d822728f61ea8a45af5d3ddc7f05d9
NagatoYuki0943 <72508155+NagatoYuki0943@users.noreply.github.com>
2025-06-27 14:52:37 +0800

    Update 11 多重装饰器.py

--- 02/day13 闭包 装饰器@/code/11 多重装饰器.py
+++ 02/day13 闭包 装饰器@/code/11 多重装饰器.py
@@ -1,52 +1,38 @@
 """
-从下到上依次装饰
-先执行 makeBold, 返回值再执行 makehItalic
-
-@ makehItalic
-@ makeBold
-def hello():
-    pass
-
+应用顺序(函数定义时):从内到外，靠近函数的装饰器先应用。
+执行顺序(函数调用时):从外到内，最外层的装饰器先执行。
 """
 
 
-def makeBold(func):
-    """文字加粗"""
+def decorator_a(func):
+    print("decorator_a")
 
-    def func_in():
-        print("文字加粗")
-        # 在hello前后加标签
-        return "<b>" + func() + "</b>"
+    def wrapper():
+        print("wrapper_a")
+        return func()
 
-    return func_in
-
-
-def makehItalic(func):
-    """文字倾斜"""
-
-    def func_in():
-        print("文字倾斜")
-        # 在hello前后加标签
-        return "<i>" + func() + "</i>"
-
-    return func_in
+    return wrapper
 
 
-# 从下到上依次装饰
-# 先执行 makeBold, 返回值再执行 makehItalic
+def decorator_b(func):
+    print("decorator_b")
+
+    def wrapper():
+        print("wrapper_b")
+        return func()
+
+    return wrapper
 
 
-@makehItalic
-@makeBold
-def hello():
-    return "hello-1"
+@decorator_a
+@decorator_b
+def my_function():
+    print("my_function")
 
 
-print(hello())
-# 文字倾斜
-# 文字加粗
-# <i><b>hello-1</b></i>
-
-# "文字倾斜"出现在"文字加粗前面", 是因为先调用了 makeBold,返回的是 func_in 函数,这个函数没有执行,
-# 而是传递给了 makehItalic函数,makehItalic中的func就是传递过来的 "func_in",然后返回它自己的"func_in",
-# 执行这个func_in的时候,先输出"文字倾斜",再调用func, 就是makeBold中的 func_in, 所以会打印 "文字加粗",最后调用hello()函数
+my_function()
+# decorator_b
+# decorator_a
+# wrapper_a
+# wrapper_b
+# my_function
