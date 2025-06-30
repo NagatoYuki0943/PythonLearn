9c6607f15aa4ed0ced08e25785c15c6e151397f5
NagatoYuki0943 <72508155+NagatoYuki0943@users.noreply.github.com>
2025-06-30 09:52:52 +0800

    Update match3.py

--- 01/match/match3.py
+++ 01/match/match3.py
@@ -41,3 +41,42 @@ match my_tp:
         print("Matched tuple t with tuple constructor")
     case list():
         print("Matched list l with list constructor")
+
+print("-" * 20)
+
+
+def check_empty_sequence1(data):
+    match data:
+        case list():
+            print("这是一个空的列表")
+        case tuple():
+            print("这是一个空的元组")
+        case [*_]:
+            print("这是一个非空序列")
+        case _:
+            print("这不是一个列表或元组")
+
+
+check_empty_sequence1([])  # 这是一个空的列表
+check_empty_sequence1(())  # 这是一个空的元组
+check_empty_sequence1([1])  # 这是一个非空序列
+check_empty_sequence1((2, 3))  # 这是一个非空序列
+check_empty_sequence1({})  # 这不是一个列表或元组
+print("-" * 20)
+
+
+def check_empty_sequence2(data):
+    match data:
+        case list() | tuple():
+            print("这是一个空的列表或元组")
+        case [*_]:
+            print("这是一个非空序列")
+        case _:
+            print("这不是一个列表或元组")
+
+
+check_empty_sequence2([])  # 这是一个空的列表或元组
+check_empty_sequence2(())  # 这是一个空的列表或元组
+check_empty_sequence2([1])  # 这是一个非空序列
+check_empty_sequence2((2, 3))  # 这是一个非空序列
+check_empty_sequence2({})  # 这不是一个列表或元组
