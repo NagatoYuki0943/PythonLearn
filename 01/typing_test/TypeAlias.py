097508a47ed6dce4f8f2a7da19f094e2d5804ee4
NagatoYuki0943 <72508155+NagatoYuki0943@users.noreply.github.com>
2026-04-13 17:50:21 +0800

    Add type annotations and new type definitions

--- 01/typing_test/TypeAlias.py
+++ 01/typing_test/TypeAlias.py
@@ -1,6 +1,7 @@
-from typing import TypeAlias
+from typing import TypeAlias, Annotated
 import numpy as np
 import torch
+from pydantic import Field
 
 
 # 最简单的写法
@@ -18,6 +19,12 @@ def retry2(url: Url2, retry_count: int) -> None: ...
 
 # 3.12 and later, 使用 type 定义类型别名
 type Url3 = str
-type array = np.ndarray | torch.Tensor
 
 def retry3(url: Url3, retry_count: int) -> None: ...
+
+# 其他用法
+type array = np.ndarray | torch.Tensor
+# type 配合 Annotated 定义带有注释的类型
+type Age = Annotated[int, "必须大于0"]
+# 结合泛型 [T] 和 Annotated，定义一个最大长度为 5 的泛型列表
+type Max5List[T] = Annotated[list[T], Field(max_length=5)]
