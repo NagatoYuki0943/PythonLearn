750a50f94294a929ffe03e30fb1bddf8fee6c5a0
NagatoYuki0943 <72508155+NagatoYuki0943@users.noreply.github.com>
2026-04-13 17:44:14 +0800

    Introduce TypeAlias for array and update type aliases
    Added TypeAlias for array and updated type definitions.

--- 01/typing_test/TypeAlias.py
+++ 01/typing_test/TypeAlias.py
@@ -1,3 +1,8 @@
+from typing import TypeAlias
+import numpy as np
+import torch
+
+
 # 最简单的写法
 Url1 = str
 
@@ -5,14 +10,14 @@ def retry1(url: Url1, retry_count: int) -> None: ...
 
 
 # 使用 TypeAlias 定义类型别名
-from typing import TypeAlias
-
 Url2: TypeAlias = str
+array: TypeAlias = np.ndarray | torch.Tensor
 
 def retry2(url: Url2, retry_count: int) -> None: ...
 
 
 # 3.12 and later, 使用 type 定义类型别名
 type Url3 = str
+type array = np.ndarray | torch.Tensor
 
 def retry3(url: Url3, retry_count: int) -> None: ...
